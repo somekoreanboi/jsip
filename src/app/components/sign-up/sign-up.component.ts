fc538a415513389021b55a4f15adae517e3eed4c
unknown <johnyim12345678@gmail.com>
2022-06-20 15:07:05 +0900

    Complete sign-up page PC design

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -38,8 +38,16 @@ export class SignUpComponent implements OnInit {
     graduationPeriod: new FormControl('', [Validators.required]),
     yearOfStudy: new FormControl('', [Validators.required]),
     japaneseProficiency: new FormControl('', [Validators.required]),
-    major: new FormControl('', [Validators.required]),
+    major: new FormControl('',),
     jobType: new FormControl('', [Validators.required]),
+    standOut: new FormControl('',),
+    reason: new FormControl('', [Validators.required]),
+    expectation: new FormControl('', [Validators.required]),
+    futureWorkplace: new FormControl('', [Validators.required]),
+    industryField: new FormControl('', [Validators.required]),
+
+
+
 
 
 
@@ -48,24 +56,18 @@ export class SignUpComponent implements OnInit {
   },)
 
 
-  industryField = this.formBuilder.group({
-    consulting: false,
-    manufacturingCompany: false,
-    IT: false,
-    foodIndustry: false,
-    media: false,
-    banking: false,
-    realEstate: false,
-    employmentAgency: false,
-    advertisingAgency: false,
-    npo: false,
-    etc: false,
-  });
-
-  countryFormControl = new FormControl();
+  countryFormControl = new FormControl('', [Validators.required])
   countryFormGroup!: FormGroup;
 
   selectedGender = 'male';
+
+  defaultNationality: Country = {
+    name: 'Singapore',
+    alpha2Code: 'SG',
+    alpha3Code: 'SGP',
+    numericCode: '702',
+    callingCode: '+65'
+  };
 
   constructor(private formBuilder: FormBuilder) {
    }
@@ -114,6 +116,54 @@ export class SignUpComponent implements OnInit {
 
   get gender() {
     return this.signUpForm.get('gender');
+  }
+
+  get yearOfStudy() {
+    return this.signUpForm.get('yearOfStudy');
+  }
+
+  get dateOfBirth() {
+    return this.signUpForm.get('dateOfBirth');
+  }
+
+  get nameOfUniversity() {
+    return this.signUpForm.get('nameOfUniversity');
+  }
+
+  get graduationPeriod() {
+    return this.signUpForm.get('graduationPeriod');
+  }
+
+  get japaneseProficiency() {
+    return this.signUpForm.get('japaneseProficiency');
+  }
+
+  get major() {
+    return this.signUpForm.get('major');
+  }
+
+  get jobType() {
+    return this.signUpForm.get('jobType');
+  }
+
+  get standOut() {
+    return this.signUpForm.get('standOut');
+  }
+
+  get reason() {
+    return this.signUpForm.get('reason');
+  }
+
+  get expectation() {
+    return this.signUpForm.get('expectation');
+  }
+
+  get futureWorkplace() {
+    return this.signUpForm.get('futureWorkplace');
+  }
+
+  get industryField() {
+    return this.signUpForm.get('industryField');
   }
 
 }
